72d3cc86fa1e52162ea25ac647ffc6a2b3b2dee6
FelipeAzeredo <felipeats2003@gmail.com>
2025-06-28 18:50:27 -0300

    ss

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./Components/form.js";
-import Grid from "./Components/grid.js";
+import Grid from "./Components/Grid";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
