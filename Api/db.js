1bee8413311021b4e26af0c9b30cf4a425b15cfa
FelipeAzeredo <felipeats2003@gmail.com>
2025-06-28 20:53:29 -0300

    teste railway e render

--- Api/db.js
+++ Api/db.js
@@ -1,8 +1,12 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "felipe123",
-    database: "crud"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
